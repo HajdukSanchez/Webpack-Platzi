b297656c3d8fb694d482fd4af1a29b54e577176d
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-05-03 12:36:00 -0500

    :art: Class

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,19 @@ module.exports = {
   // Extensions that he need to work with
   resolve: {
     extensions: ['.js'] // Array of extension that webpack needs to identify in our project
-  }
+  },
+  // When we are going to add the configuration of babel plugin for JS
+  module: {
+    // Configurations for modules
+    rules: [
+      // Object for work with babel loader and connect our webpack with babel
+      {
+        test: /\.m?js$/, // This expression are going to tell us how can i work with diferents extensions
+        exclude: /node_modules/,  // To tell him that not use NodeModules JS files
+        use: {
+          loader: 'babel-loader' // The loader that he going to use
+        }
+      }
+    ]
+  },
 }
